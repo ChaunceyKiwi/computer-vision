a1cfc4d160272009f5fb12ae4f33c36206f5827d
Chauncey <chaunceykiwi@gmail.com>
2016-02-01 20:11:05 -0800

    add code of umfpack part...

--- ImageMatting/ImageProcessing/ImageProcessing/ViewController.mm
+++ ImageMatting/ImageProcessing/ImageProcessing/ViewController.mm
@@ -365,6 +365,45 @@ std::vector<T> tripletList;
     return A;
 }
 
+//- (SpMat) DemoTry
+//{
+//    SpMat A;
+//    
+//    int n = 5;
+//    double x[5];
+//    void *Symbolic, *Numeric;
+//    int i;
+//    
+//    /* cumulative count of entries, as matrix is scanned columnwise */
+//    int Ap[] = { 0, 2, 5, 9, 10, 12 };
+//    
+//    /* row indices of entries, as matrix is scanned columnwise */
+//    int Ai[] =    { 0, 1, 0,  2, 4, 1,  2, 3, 4, 2, 1, 4 };
+//    
+//    /* matrix entries */
+//    double Ax[] = { 2, 3, 3, -1, 4, 4, -3, 1, 2, 2, 6, 1 };
+//    
+//    /* the right hand side */
+//    double b[] = { 8, 45, -3, 3, 19 };
+//    
+//    /* symbolic analysis */
+//    umfpack_di_symbolic(n, n, Ap, Ai, Ax, &Symbolic, NULL, NULL);
+//    
+//    /* LU factorization */
+//    umfpack_di_numeric(Ap, Ai, Ax, Symbolic, &Numeric, NULL, NULL);
+//    umfpack_di_free_symbolic(&Symbolic);
+//    
+//    /* solve system */
+//    umfpack_di_solve(UMFPACK_A, Ap, Ai, Ax, x, b, Numeric, NULL, NULL);
+//    umfpack_di_free_numeric(&Numeric);
+//    
+//    for (i = 0; i < n; i++)
+//        printf("x[%d] = %g\n", i, x[i]);
+//    
+//    return A;
+//    
+//}
+
 - (cv::Mat)cvMatFromUIImage:(UIImage *)image
 {
     CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
