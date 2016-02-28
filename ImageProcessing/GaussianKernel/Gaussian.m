be39dd7463b59e1b17134ca715ebfcc28faa1b80
Chauncey <chaunceykiwi@gmail.com>
2016-02-27 18:28:28 -0800

    Gaussian Kernel

--- ImageProcessing/GaussianKernel/Gaussian.m
+++ ImageProcessing/GaussianKernel/Gaussian.m
@@ -1,16 +1,19 @@
 %Blur with Gaussian kernel
-size = 10; % The size of kernel
 img = imread('Lenna.png');
-img = rgb2gray(img)/255;
+img = double(rgb2gray(img))/255;
 t = 10; %Scale 
-
+size = 2*t; % The size of kernel
 imgBlur = zeros(size,size);
 
-%build Gaussian kernel
+%build Gaussian kernel and check sum
 for x = -size:size
     for y = -size:size
         imgBlur(x+size+1,y+size+1) = 1/(2*pi*t)*(exp((x.^2+y.^2)/(-2*t)));
     end
+end
+
+if(abs(sum(imgBlur(:)) - 1) > 1e-03)
+    disp('Kernel range too small!');
 end
 
 %convolution with image using kernel
@@ -22,8 +25,8 @@ Z = imgBlur;
 figure
 surface(X,Y,Z);
 view(3)
-saveas(figure(1), 'Kernel','bmp');
+saveas(figure(1), 'Kernel','jpg');
 
 %image of Lenna after convolution
-imgBlur = imshow(imgGaussian);
-imwrite(imgGaussian,'imgBlur.bmp');
+imtool(imgGaussian);
+imwrite(imgGaussian,'imgBlur.jpg');
