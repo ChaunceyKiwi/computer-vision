34d7797db0dbae9f87c179543d5c6d8325dd2b62
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-02-26 22:29:24 -0800

    testK with bounding box added

--- generalized-hough-transform/test_K/main.m
+++ generalized-hough-transform/test_K/main.m
@@ -2,12 +2,14 @@ template = imread('./PA2-testimages/template_K.png');
 testImage = imread('./PA2-testimages/letters.png');
 [rTable, bin_count] = build_RTable(template);
 
+[h, w, ~] = size(template);
 imshow(testImage);
 hold on;
 % rotate test image with counter-clockwise angle
 for angle = 1 : 4 : 360
     pos = matching(testImage, rTable, bin_count, angle);
     for i = 1 : size(pos, 1)
-        plot(pos(i, 2), pos(i, 1),'r.','MarkerSize', 20);
+        rectangle('Position', [pos(i, 2) - w / 2, pos(i, 1) - h / 2, w, h],...
+	'EdgeColor','r', 'LineWidth', 3)
     end
 end
